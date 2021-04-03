f8116941b06944326e62e43bf8a84039f700d3c8
snowden96 <moneem.benbelgasem@esen.tn>
2021-04-03 16:09:58 +0200

    methode setup et chargement des variable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 ref="titre">this is home page</h1>
+    <p>
+      my name is :{{ myname }} <br />
+      et mon ecole est : {{ monecole }}
+    </p>
+    <button @click="sayhello">marhaba</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    let name = 'moneam';
+    let ecole = 'ESEN';
+    let titre = ref(null);
+
+    const sayhello = () => {
+      console.log(titre.value);
+      titre.value.classList.add('my-4');
+      titre.value.textContent = 'welcome to my web site';
+    };
+
+    return {
+      myname: name,
+      monecole: ecole,
+      sayhello,
+      titre,
+    };
+  },
+};
 </script>
