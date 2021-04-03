93903a86945ac2354f977a33c94077206777e8d9
snowden96 <moneem.benbelgasem@esen.tn>
2021-04-04 01:50:08 +0200

    using route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import Show from '../views/Show.vue';
 import Posts from '../views/Posts.vue';
 
 const routes = [
@@ -10,14 +11,19 @@ const routes = [
     component: Home,
   },
   {
-    path: '/blog',
-    name: 'Blog',
-    component: Posts,
-  },
-  {
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/blog/:id',
+    name: 'Show',
+    component: Show,
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Posts,
   },
 ];
 
